3157ff92a6bc1032dfe6db458b673c6dbd7a3da6
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2020-12-02 18:51:44 -0800

    TST improve test_suppress_validation

--- himalaya/tests/test_validation.py
+++ himalaya/tests/test_validation.py
@@ -2,11 +2,15 @@ import numpy as np
 import sklearn
 import pytest
 
+from himalaya.backend import set_backend
+from himalaya.backend import ALL_BACKENDS
 from himalaya.validation import _assert_all_finite
 
 
-def test_suppress_validation():
-    X = np.array([0, np.inf])
+@pytest.mark.parametrize('backend', ALL_BACKENDS)
+def test_suppress_validation(backend):
+    backend = set_backend(backend)
+    X = backend.asarray([0, np.inf])
     with pytest.raises(ValueError):
         _assert_all_finite(X, True)
     sklearn.set_config(assume_finite=True)
