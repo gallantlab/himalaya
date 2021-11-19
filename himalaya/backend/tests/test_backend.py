cbbd0248e67b2c2d969e21e85afa6f421659a36a
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2021-11-18 17:18:16 -0800

    TST add test for force_cpu decorator

--- himalaya/backend/tests/test_backend.py
+++ himalaya/backend/tests/test_backend.py
@@ -1,39 +1,9 @@
 import pytest
 
 from himalaya.backend import set_backend
-from himalaya.backend import get_backend
 from himalaya.backend import ALL_BACKENDS
 from himalaya.backend._utils import _dtype_to_str
 from himalaya.utils import assert_array_almost_equal
-
-
-@pytest.mark.parametrize('backend', ALL_BACKENDS)
-def test_set_backend_correct(backend):
-    # test the change of backend
-    module = set_backend(backend)
-    assert module.__name__.split('.')[-1] == backend
-
-    # test idempotence
-    module = set_backend(set_backend(backend))
-    assert module.__name__.split('.')[-1] == backend
-
-    # test set and get
-    module = set_backend(get_backend())
-    assert module.__name__.split('.')[-1] == backend
-
-    assert set_backend(backend)
-
-
-def test_set_backend_incorrect():
-    for backend in ["wrong", ["numpy"], True, None, 10]:
-        with pytest.raises(ValueError):
-            set_backend(backend)
-        with pytest.raises(ValueError):
-            set_backend(backend, on_error="raise")
-        with pytest.warns(Warning):
-            set_backend(backend, on_error="warn")
-        with pytest.raises(ValueError):
-            set_backend(backend, on_error="foo")
 
 
 @pytest.mark.parametrize('backend', ALL_BACKENDS)
